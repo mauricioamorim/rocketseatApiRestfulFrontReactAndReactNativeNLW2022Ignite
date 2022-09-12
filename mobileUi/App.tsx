df7a160e0300e60709df654567604c1b72520d0b
Mauricio <mauricioamorim.contato@gmail.com>
2022-09-12 17:38:57 -0300

    mobileUi core

---
+++ mobileUi/App.tsx
@@ -0,0 +1,37 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Hello React Native!</Text>
+      <Button title='Send 1'></Button>
+      <StatusBar style="auto" backgroundColor='blue' />
+    </View>
+  );
+}
+
+interface ButtonProps {
+  title: string;
+}
+function Button(props: ButtonProps) {
+  return (
+    <TouchableOpacity>
+      <Text>
+        {props.title}
+      </Text>
+    </TouchableOpacity>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    backgroundColor : 'blue'
+  }
+});
