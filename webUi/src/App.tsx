84dc27193e62255993eeb9539a6241136e0d6aef
Mauricio <mauricioamorim.contato@gmail.com>
2022-09-13 13:24:17 -0300

    webUi DIa 2

--- webUi/src/App.tsx
+++ webUi/src/App.tsx
@@ -2,6 +2,15 @@
    JSK = JavaScript + XML(HTML)
 */
 
+import {MagnifyingGlassPlus} from 'phosphor-react'
+
+
+/* importando css */
+import './styles/main.css';
+
+/* importando a imagem para variavel logo */
+import logoImg from './assets/logo-nlw-esports.svg';
+
 /*
    Componentes e Propriedades
 
@@ -9,15 +18,22 @@
    <img src="">
 */
 // exemplo de um componente
-interface ButtonProps {
+interface GamesProps {
   title: string;
+  link: string;
+  imgSrc: string;
+  quantaty: string;
 }
-
-function Button(props: ButtonProps) {
+function Games(props: GamesProps) {
   return (
-    <button>
-      {props.title}
-    </button>
+    <a href="{props.title}" className='relative rounded-lg overflow-hidden'>
+      <img src={props.imgSrc} alt={props.title} />
+
+      <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
+        <strong className='font-bold text-white block'>{props.title}</strong>
+        <span className='text-zinc-300 text-sm'>{props.quantaty} anúncios</span>
+      </div>
+    </a>
   )
 }
 
@@ -25,12 +41,35 @@ function Button(props: ButtonProps) {
 function App() {
   // é obrigatório ter os elementos encapsulados pela div
   return (
-    <div>
-      <Button title="Send 1" />
-      <Button title="Send 2" />
-      <Button title="Send 3" />
+    <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
+      <img src={logoImg} alt="" />
 
-      <h1>Hello NLW!!!</h1>
+      <h1 className="text-6xl text-white font-black mt-20">
+        Seu <span className='text-transparent bg-nlw-gradient bg-clip-text'>duo</span> está aqui
+      </h1>
+
+      <div className='grid grid-cols-6 gap-6 mt-16'>
+        <Games title="game 1" imgSrc='./game-1.png' quantaty="4" link="#" />
+        <Games title="game 2" imgSrc='./game-2.png' quantaty="0" link="#" />
+        <Games title="game 3" imgSrc='./game-3.png' quantaty="0" link="#" />
+        <Games title="game 4" imgSrc='./game-4.png' quantaty="0" link="#" />
+        <Games title="game 5" imgSrc='./game-5.png' quantaty="0" link="#" />
+        <Games title="game 6" imgSrc='./game-6.png' quantaty="0" link="#" />
+      </div>
+
+      <div className='pt-1 bg-nlw-gradient self-stretch rounded-lg mt-8 overflow-hidden'>
+        <div className='bg-[#2A2634] px-8 py-6 flex justify-between items-center'>
+          <div>
+            <strong className='text-2xl text-white font-black block'>Não encontrou seu duo?</strong>
+            <span className='text-zinc-400 block'>Publique um anúncio para encontrar novos players!</span>
+          </div>
+
+          <button className='py-3 px-4 bg-violet-500 hover:bg-violet-600 text-white rounded flex items-center gab-3'>
+            <MagnifyingGlassPlus size={24} />
+            Publicar anúncio
+          </button>
+        </div>
+      </div>
     </div>
   )
 }
